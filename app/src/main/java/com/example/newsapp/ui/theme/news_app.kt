463c78980554e82ce6162a10637addcd738ff438
Paras Arora <parasarora1224@gmail.com>
2023-07-15 11:51:23 +0530

    added bottom navigation bar

--- app/src/main/java/com/example/newsapp/ui/theme/news_app.kt
+++ app/src/main/java/com/example/newsapp/ui/theme/news_app.kt
@@ -4,36 +4,70 @@ import com.example.newsapp.ui.theme.screen.DetailScreen
 import com.example.newsapp.ui.theme.screen.TopNews
 
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavType
+import androidx.navigation.*
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
+import com.example.newsapp.BottomMenuScreen
+import com.example.newsapp.components.BottomMenu
 
+//import eu.tutorials.newsapp.BottomMenuScreen
+//import eu.tutorials.newsapp.MockData
+//import eu.tutorials.newsapp.components.BottomMenu
+//import eu.tutorials.newsapp.ui.screen.Categories
+//import eu.tutorials.newsapp.ui.screen.DetailScreen
+//import eu.tutorials.newsapp.ui.screen.Sources
+//import eu.tutorials.newsapp.ui.screen.TopNews
 
 @Composable
 fun NewsApp() {
-    Navigation()
-}
-
-@Composable
-fun Navigation() {
     val scrollState = rememberScrollState()
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "TopNews") {
-        composable("TopNews") {
-            TopNews(navController = navController)
-        }
+    //Todo 8: set MainScreen in the NewsApp
+    MainScreen(navController = navController,scrollState)
+}
+
+//Todo 1: create a mainScreen composable
+@Composable
+fun MainScreen(navController: NavHostController,scrollState: ScrollState) {
+    Scaffold(bottomBar ={
+        BottomMenu(navController = navController)
+    }) {it
+        //Todo 9: set Navigation in the MainScreen
+        Navigation(navController =navController , scrollState =scrollState )
+    }
+}
+
+
+@Composable
+fun Navigation(navController:NavHostController,scrollState: ScrollState) {
+    NavHost(navController = navController, startDestination = BottomMenuScreen.TopNews.route) {
+        //Todo 13: Add the bottom navigation to the Navigation graph
+        bottomNavigation(navController = navController)
         composable("Detail/{newsId}",
             arguments = listOf(
                 navArgument("newsId") { type = NavType.IntType }
             )){navBackStackEntry->
             val id = navBackStackEntry.arguments?.getInt("newsId")
             val newsData = MockData.getNews(id)
-            //Todo 7: provide value for navController
             DetailScreen(newsData,scrollState,navController)
         }
+    }
+}
+
+//Todo 12: create bottom navigation function for the three options
+fun NavGraphBuilder.bottomNavigation(navController: NavController) {
+    composable(BottomMenuScreen.TopNews.route) {
+        TopNews(navController = navController)
+    }
+    composable(BottomMenuScreen.Categories.route) {
+        BottomMenuScreen.Categories
+    }
+    composable(BottomMenuScreen.Sources.route) {
+        BottomMenuScreen.Sources
     }
 }
